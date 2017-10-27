db5e9689d6254e3f318fcb63192f3d081f5e6e77
Lindsey-Halliday <linzehalliday@gmail.com>
2017-10-27 16:20:22 -0700

    add conditional statements for questions

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,43 @@ $(document).ready(function() {
     var q2 = parseInt($("#family").val());
     var q3 = parseInt($("#lodging").val());
     var q4 = parseInt($("#crowds").val());
+
+    if (q1 == 1) {
+      dest1 += 1;
+    } else if (q1 == 2) {
+      dest2 += 1;
+    } else if (q1 == 3) {
+      dest3 += 1;
+    } else if (q1 == 4) {
+      dest4 += 1;
+    }
+
+    if (q2 == 1) {
+      dest1 += 1;
+    } else if (q2 == 2) {
+      dest2 += 1;
+    } else if (q2 == 3) {
+      dest3 += 1;
+    } else if (q2 == 4) {
+      dest4 += 1;
+    }
+
+    if (q3 == 1) {
+      dest1 += 1;
+    } else if (q3 == 2) {
+      dest2 += 1;
+    } else if (q3 == 3) {
+      dest3 += 1;
+    } else if (q3 == 4) {
+      dest4 += 1;
+    }
+
+    if (q4 == 1) {
+      dest1 += 1;
+    } else if (q4 == 2) {
+      dest2 += 1;
+    } else if (q4 == 3) {
+      dest3 += 1;
+    } else if (q4 == 4) {
+      dest4 += 1;
+    }
